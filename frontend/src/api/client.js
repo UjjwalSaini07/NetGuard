911cbb0b15dc6dff53d4675addf6b4339f40a25f
Ujjwal Saini <ujjwalsaini1947@gmail.com>
2026-08-21 23:34:23 +0530

    Added Secure layer

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,10 +1,18 @@
 import axios from 'axios'
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000'
+const apiKey = import.meta.env.VITE_API_KEY || 'netguard-secret-api-key-2026'
+
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL,
   headers: {
-    'x-api-key': import.meta.env.VITE_API_KEY
+    'x-api-key': apiKey
   }
+})
+
+client.interceptors.request.use((config) => {
+  config.headers['x-api-key'] = import.meta.env.VITE_API_KEY || apiKey
+  return config
 })
 
 export default client
